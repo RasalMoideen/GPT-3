cbed98e603a338f44147aa0d9b15a2e1a3245b27
RasalMoideen <rasalmoideen4646@gmail.com>
2023-02-01 13:05:01 +0530

    first commit 1st

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+function toggleFunction(){
+    let navItem = document.querySelector(".menu_items")
+    let toggleBtn = document.querySelector(".toggle-btn")
+    navItem.classList.toggle("show_menu")
+    toggleBtn.classList.toggle("animation")
+}
+
+
